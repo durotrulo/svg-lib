7ec9095cd717fcaab6ac3e6bfdf457907165c196
Ciki <matula.m@gmail.com>
2011-05-22 19:32:29 +0200

    fix options toggler's width in chrome

--- www/modules/history.js/history.js
+++ www/modules/history.js/history.js
@@ -1,5 +1,6 @@
 /**
  * History.js Core
+ * @see https://github.com/balupton/History.js
  * @author Benjamin Arthur Lupton <contact@balupton.com>
  * @copyright 2010-2011 Benjamin Arthur Lupton <contact@balupton.com>
  * @license New BSD License <http://creativecommons.org/licenses/BSD/>
